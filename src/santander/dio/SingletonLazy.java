4071555ae4ed04edbcd0b62ffcbf6e32c30786f2
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-13 16:18:07 -0300

    new package: santander.dio; new classes: SingletonLazy, SingletoEager

---
+++ src/santander/dio/SingletonLazy.java
@@ -0,0 +1,18 @@
+package santander.dio;
+
+/*Singleton Preguiçoso*/
+
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if(instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
